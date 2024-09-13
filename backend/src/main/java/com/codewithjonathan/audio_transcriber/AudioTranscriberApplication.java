55cf2e5f67ab06a314e2cc57440fe32ce67521c2
jonathantong <jontong321@gmail.com>
2024-09-13 09:41:51 -0400

    Initial commit with backend folders

---
+++ backend/src/main/java/com/codewithjonathan/audio_transcriber/AudioTranscriberApplication.java
@@ -0,0 +1,13 @@
+package com.codewithjonathan.audio_transcriber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudioTranscriberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudioTranscriberApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/codewithjonathan/audio_transcriber/AudioTranscriberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithjonathan.audio_transcriber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudioTranscriberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
